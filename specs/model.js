285fda5d6a713434eaac3022d05bd74198508018
Jeff Haynie <jhaynie@appcelerator.com>
2014-09-08 20:10:56 -0700

    add connector level metadata

--- specs/model.js
+++ specs/model.js
@@ -650,4 +650,85 @@ describe('models',function(){
 
 	});
 
+
+	describe('#metadata', function(){
+
+		it('should be able to fetch no metadata', function(){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector
+			});
+
+			should(User.getMeta('foo')).be.null;
+		});
+
+		it('should be able to fetch default', function(){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector
+			});
+
+			should(User.getMeta('foo','bar')).be.equal('bar');
+		});
+
+		it('should be able to fetch from Model', function(){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector,
+				metadata: {
+					memory: {
+						foo: 'bar'
+					}
+				}
+			});
+
+			should(User.getMeta('foo')).be.equal('bar');
+		});
+
+		it('should be able to set on Model', function(){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector,
+				metadata: {
+					memory: {
+						foo: 'bar'
+					}
+				}
+			});
+
+			User.setMeta('foo','bar2');
+
+			should(User.getMeta('foo')).be.equal('bar2');
+		});
+
+	});
+
 });
