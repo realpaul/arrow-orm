5bc6bb42d45c47352954820222ad2b8c2042e035
Jeff Haynie <jhaynie@appcelerator.com>
2014-12-10 20:42:45 -0800

    filter model methods based on connector implementation

--- specs/model.js
+++ specs/model.js
@@ -1640,4 +1640,46 @@ describe('models',function(){
 
 	});
 
+	describe("#operations" ,function() {
+
+		it("should define model function based on connector", function(){
+			var Connector = new orm.MemoryConnector();
+			Connector.deleteAll = null;
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector,
+				metadata: {
+					memory: {
+						foo: 'bar'
+					}
+				}
+			});
+			should(User.deleteAll).be.Undefined;
+			should(User.removeAll).be.Function;
+
+			Connector.create = null;
+			var User2 = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: Connector,
+				metadata: {
+					memory: {
+						foo: 'bar'
+					}
+				}
+			});
+			should(User2.create).be.Undefined;
+		});
+
+	});
+
 });
