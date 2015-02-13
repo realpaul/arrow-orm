9c7587c8b050e5f61b7f09fd164da593e63397d5
Jeff Haynie <jhaynie@appcelerator.com>
2015-02-13 00:12:28 -0800

    0.2.17 - added test cases for sel, unsel. fixed unself for memory connector

--- specs/model.js
+++ specs/model.js
@@ -2192,6 +2192,116 @@ describe('models',function(){
 
 	});
 
+	describe('#query', function() {
+
+		it('should support query with sel', function(callback){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					},
+					email: {
+						type: String
+					}
+				},
+				connector: Connector
+			});
+			User.create({name:'jeff',age:25});
+			User.create({name:'nolan',age:55});
+			User.create({name:'neeraj',age:35});
+			User.query({where:{age:{$gte:30}},sel:'name,age'},function(err,collection) {
+				should(err).not.be.ok;
+				should(collection).be.an.object;
+				should(collection.length).be.equal(2);
+				var record = collection[0].toJSON();
+				should(record).have.property('name');
+				should(record).have.property('age');
+				should(record).not.have.property('email');
+				callback();
+			});
+		});
+
+		it('should support query with multiple sel fields', function(callback){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					type: {type:String},
+					description: {type:String},
+					name: {type:String},
+					author: {type:String},
+					author_username: {type:String},
+					author_id: {type:String},
+					component: {type: String},
+					version: {type: String}
+				},
+				connector: Connector
+			});
+			User.create({name:'jeff',description:'cool dude',type:'janitor',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.create({name:'nolan',description:'whaaat?',type:'manager',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.create({name:'dawson',description:'awesome, dawson',type:'coder',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.query({where:{},sel:'description,type,name,author,author_username,author_id'},function(err,collection) {
+				should(err).not.be.ok;
+				should(collection).be.an.object;
+				should(collection.length).be.equal(3);
+				var record = collection[0].toJSON();
+				should(record).have.property('description');
+				should(record).have.property('type');
+				should(record).have.property('name');
+				should(record).have.property('author_username');
+				should(record).have.property('author');
+				should(record).have.property('author_id');
+				should(record).not.have.property('version');
+				should(record).not.have.property('component');
+				callback();
+			});
+		});
+
+		it('should support query with unsel fields', function(callback){
+			var Connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user',{
+				fields: {
+					type: {type:String},
+					description: {type:String},
+					name: {type:String},
+					author: {type:String},
+					author_username: {type:String},
+					author_id: {type:String},
+					component: {type: String},
+					version: {type: String}
+				},
+				connector: Connector
+			});
+			User.create({name:'jeff',description:'cool dude',type:'janitor',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.create({name:'nolan',description:'whaaat?',type:'manager',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.create({name:'dawson',description:'awesome, dawson',type:'coder',author:'blah',author_username:'test@example.com',author_id:'123'});
+			User.query({where:{},unsel:'component,version'},function(err,collection) {
+				should(err).not.be.ok;
+				should(collection).be.an.object;
+				should(collection.length).be.equal(3);
+				var record = collection[0].toJSON();
+				should(record).have.property('description');
+				should(record).have.property('type');
+				should(record).have.property('name');
+				should(record).have.property('author_username');
+				should(record).have.property('author');
+				should(record).have.property('author_id');
+				should(record).not.have.property('version');
+				should(record).not.have.property('component');
+				callback();
+			});
+		});
+
+	});
+
 	describe('#count', function(){
 
 		it('should return count', function(callback) {
