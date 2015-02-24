91be41a25d9d1613359a697bd605ca5e49c1ae83
Jeff Haynie <jhaynie@appcelerator.com>
2015-02-24 15:06:30 -0800

    API-519 support for model level transformations using serialize/deserialize

--- specs/model.js
+++ specs/model.js
@@ -2532,6 +2532,161 @@ describe('models',function(){
 
 	});
 
+	describe('#serialize', function() {
+
+		it('should support custom serializer with no changes', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				serialize: function(obj, instance, model) {
+					return obj;
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toJSON();
+			should(obj).have.property('name','jeff');
+			should(obj).have.property('age',25);
+		});
+
+		it('should support custom serializer returning undefined', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				serialize: function(obj, instance, model) {
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toJSON();
+			should(obj).be.undefined;
+		});
+
+		it('should support custom serializer returning a new property', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				serialize: function(obj, instance, model) {
+					obj.foo = 'bar';
+					return obj;
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toJSON();
+			should(obj).have.property('name','jeff');
+			should(obj).have.property('age',25);
+			should(obj).have.property('foo','bar');
+		});
+
+		it('should support custom serializer returning a new property from the instance', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				serialize: function(obj, instance, model) {
+					obj.foo = instance.get('name');
+					return obj;
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toJSON();
+			should(obj).have.property('name','jeff');
+			should(obj).have.property('age',25);
+			should(obj).have.property('foo','jeff');
+		});
+
+	});
+
+	describe('#deserialize', function() {
+
+		it('should support custom deserializer with no changes', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				deserialize: function(obj, instance, model) {
+					return obj;
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toPayload();
+			should(obj).have.property('name','jeff');
+			should(obj).have.property('age',25);
+		});
+
+		it('should support custom deserializer new prop', function(){
+			var Connector = new orm.MemoryConnector();
+			var User = orm.Model.define('user',{
+				fields: {
+					name: {
+						type: String,
+						required: true
+					},
+					age: {
+						type: Number,
+						required: true
+					}
+				},
+				connector: Connector,
+				deserialize: function(obj, instance, model) {
+					obj.foo = instance.get('name');
+					return obj;
+				}
+			});
+			var instance = User.instance({name:'jeff',age:25});
+			var obj = instance.toPayload();
+			should(obj).have.property('name','jeff');
+			should(obj).have.property('age',25);
+			should(obj).have.property('foo','jeff');
+		});
+	});
+
 	describe('#count', function(){
 
 		it('should return count', function(callback) {
