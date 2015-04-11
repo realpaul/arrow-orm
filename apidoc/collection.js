de0d8409aaf0b34e56d07af0e1c969a1156590aa
Ben Hatfield <bhatfield@appcelerator.com>
2015-04-10 18:17:11 -0700

    [TIDOC-2178] Let there be docs

---
+++ apidoc/collection.js
@@ -0,0 +1,16 @@
+/**
+ * @class Arrow.Collection
+ * An Arrow Collection is an array of Arrow Model instances with some logic
+ * to manipulate the models.
+ *
+ * To create a Collection, load the `arrow` module, then use the Collection constructor
+ * to create an instance of a Collection. You can optionally pass in an array of Model instances
+ * to initialize the collection.
+ *
+ *     var Arrow = require('arrow'),
+ *         Collection = Arrow.Collection,
+ *         myModels = new Collection([Model instances...]);
+ *     ...
+ *     var lastModel = myModels.slice(-1);
+ *
+ */
